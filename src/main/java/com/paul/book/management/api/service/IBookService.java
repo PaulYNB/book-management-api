97ad99282d8064b60a22a55e91215e1bc8c5ea52
git-paul <464482875@qq.com>
2024-03-22 17:01:08 +0800

    Throws DuplicateKeyException on IBookService.createBook(Book).

--- src/main/java/com/paul/book/management/api/service/IBookService.java
+++ src/main/java/com/paul/book/management/api/service/IBookService.java
@@ -2,13 +2,15 @@ package com.paul.book.management.api.service;
 
 import java.util.List;
 
+import org.springframework.dao.DuplicateKeyException;
+
 import com.paul.book.management.api.entity.Book;
 
 public interface IBookService {
-	public void createBook(Book book);
+	public int createBook(Book book) throws DuplicateKeyException;
 	public List<Book> getBooks(String bookId);
 	public Book getBook(String bookId);
 	public List<Book> getAllBooks();
-	public void updateBook(Book book);
-	public void deleteBook(String bookId);
+	public int updateBook(Book book);
+	public int deleteBook(String bookId);
 }
